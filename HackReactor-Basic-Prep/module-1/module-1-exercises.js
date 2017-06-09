b7ca266d53fcccd1338bc6c19e4c09cf36f280f6
SerCJm <scjmoro@gmail.com>
2017-06-09 09:30:52 -0700

    Add exercises to module 1 exercises 89 to 144

--- HackReactor-Basic-Prep/module-1/module-1-exercises.js
+++ HackReactor-Basic-Prep/module-1/module-1-exercises.js
@@ -606,3 +606,591 @@ function select(arr, obj) {
 }
 
 // exercise 89
+function getElementsLessThan100AtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (obj[key]) {
+    for (i = 0; i < obj[key].length; i++) {
+      if (obj[key][i] < 100) {
+        result.push(obj[key][i]);
+      }
+    }
+  }
+  return result;
+}
+
+function countAllCharacters(str) {
+  // your code here
+  var result = {};
+  if (str) {
+    for (i = 0; i < str.length; i++) {
+      if (result[str[i]]) {
+        result[str[i]] = result[str[i]] + 1;
+      } else {
+        result[str[i]] = 1;
+      }
+    }
+  }
+  return result;
+}
+
+/* function getElementsGreaterThan10AtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (obj[key]) {
+    for (i = 0; i < obj[key].length; i++) {
+      if (obj[key][i] > 10) {
+        result.push(obj[key][i]);
+      }
+    }
+  }
+  return result;
+}
+*/
+
+function removeElement(array, discarder) {
+  // your code here
+  var result = array.filter(element => element !== discarder);
+  return result;
+}
+
+function getFirstElementOfProperty(obj, key) {
+  // your code here
+  if (obj[key] instanceof Array) {
+    return obj[key][0];
+  }
+}
+
+function getNthElementOfProperty(obj, key, n) {
+  // your code here
+  if (obj[key] instanceof Array) {
+    return obj[key][n];
+  }
+}
+
+function getLastElementOfProperty(obj, key) {
+  // your code here
+  if (obj[key] instanceof Array) {
+    return obj[key][obj[key].length - 1];
+  }
+}
+
+function keep(array, keeper) {
+  // your code here
+  var result = array.filter(element => element === keeper);
+  return result;
+}
+
+function getOddLengthWordsAtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (obj[key] instanceof Array) {
+    for (i = 0; i < obj[key].length; i++) {
+      if (obj[key][i].length%2 === 1) {
+        result.push(obj[key][i]);
+      }
+    }
+  }
+  return result;
+}
+
+function computeAverageOfNumbers(nums) {
+  // your code here
+  var result = 0;
+  if (nums.length > 0) {
+    nums.forEach(element => result += element);
+    return (result/nums.length);
+  } else {
+    return 0;
+  }
+}
+
+function getAverageOfElementsAtProperty(obj, key) {
+  // your code here
+  if (obj[key] instanceof Array && obj[key].length > 0) {
+    var result = 0;
+    obj[key].forEach(element => result += element);
+    return (result/obj[key].length);
+  } else {
+    return 0;
+  }
+}
+
+function getEvenLengthWordsAtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (Array.isArray(obj[key])) {
+    for (i = 0; i < obj[key].length; i++) {
+      if (obj[key][i].length%2 === 0) {
+        result.push(obj[key][i]);
+      }
+    }
+  }
+  return result;
+}
+
+function filterOddLengthWords(words) {
+  // your code here
+  var result = words.filter(word => word.length%2 !== 0);
+  return result;
+}
+
+function getSquaredElementsAtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (Array.isArray(obj[key])) {
+    for (i = 0; i < obj[key].length; i++) {
+      result.push(obj[key][i] ** 2);
+    }
+  }
+  return result;
+}
+
+function getOddElementsAtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (Array.isArray(obj[key])) {
+    for (i = 0; i < obj[key].length; i++) {
+      if (obj[key][i]%2 === 1) {
+        result.push(obj[key][i]);
+      }
+    }
+  }
+  return result;
+}
+
+function getEvenElementsAtProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (Array.isArray(obj[key])) {
+    for (i = 0; i < obj[key].length; i++) {
+      if (obj[key][i]%2 === 0) {
+        result.push(obj[key][i]);
+      }
+    }
+  }
+  return result;
+}
+
+function filterEvenLengthWords(words) {
+  // your code here
+  var result = words.filter(word => word.length%2 === 0);
+  return result;
+}
+
+function getLengthOfLongestElement(arr) {
+  // your code here
+  var result = 0;
+  arr.forEach(function (ele) {
+    if (ele.length > result) {
+      result = ele.length;
+    }
+  })
+  return result;
+}
+
+function getSmallestElementAtProperty(obj, key) {
+  // your code here
+  if (Array.isArray(obj[key])) {
+  var result = obj[key][0];
+    obj[key].forEach((ele) => {
+      if (ele < result) {
+        result = ele
+      }
+    });
+  return result;
+  }
+}
+
+function getLargestElementAtProperty(obj, key) {
+  // your code here
+  var result;
+  if (Array.isArray(obj[key]) && obj[key].length > 0) {
+    result = Math.max(...obj[key])
+  }
+  return result;
+}
+
+function getAllButLastElementOfProperty(obj, key) {
+  // your code here
+  var result = [];
+  if (Array.isArray(obj[key])) {
+    result = obj[key].slice(0, obj[key].length - 1);
+  }
+  return result;
+}
+
+function getElementOfArrayProperty(obj, key, index) {
+  // your code here
+  var result;
+  if (Array.isArray(obj[key])) {
+    result = obj[key][index];
+  }
+  return result;
+}
+
+/* function squareElements(arr) {
+  // your code here
+  var result = [];
+  for (i = 0; i < arr.length; i++) {
+    result.push(arr[i] ** 2);
+  }
+  return result;
+}
+
+*/
+
+function squareElements(arr) {
+  // your code here
+  return arr.map(e => e ** 2);
+}
+
+function filterOddElements(arr) {
+  // your code here
+  return arr.filter(e => e%2 === 1);
+}
+
+function computeProductOfAllElements(arr) {
+  // your code here
+  if (arr.length > 0) {
+    return arr.reduce((pr, e) => pr * e);
+  } else {
+    return 0;
+  }
+}
+
+function filterEvenElements(arr) {
+  // your code here
+  return arr.filter(e => e%2 === 0);
+}
+
+function getLengthOfShortestElement(arr) {
+  // your code here
+  if (arr.length > 0) {
+    var lengths = arr.map(word => word.length);
+    return Math.min(...lengths);
+  } else {
+    return 0;
+  }
+}
+
+function getLongestElement(arr) {
+  // your code here
+  if (arr.length > 0) {
+    var lengths = arr.map(w => w.length);
+    var result = arr.filter(w => w.length === Math.max(...lengths));
+    return result[0];
+  } else {
+    return '';
+  }
+}
+
+function findSmallestElement(arr) {
+  // your code here
+  if (arr.length > 0) {
+    return Math.min(...arr);
+  } else {
+    return 0;
+  }
+}
+
+function findShortestElement(arr) {
+  // your code here
+  var smallest = '';
+  if (arr.length > 0) {
+    smallest = arr[0];
+    arr.filter(w => {
+      if (w.length < smallest.length) {
+        smallest = w;
+      }
+    });
+  }
+  return smallest;
+}
+
+function getLargestElement(arr) {
+  // your code here
+  return Math.max(...arr);
+}
+
+function computeSumOfAllElements(arr) {
+  // your code here
+  return arr.reduce((acc, val) => acc + val, 0);
+}
+
+function calculateBillTotal(preTaxAndTipAmount) {
+  // your code here
+  const TAX = 1.095;
+  return ((preTaxAndTipAmount * TAX) + (preTaxAndTipAmount * .15));
+}
+
+function getStringLength(string) {
+  // your code here
+  var count = 0;
+  while (string) {
+    string = string.slice(count);
+    count++;
+  }
+  return count;
+}
+
+function joinArrayOfArrays(arr) {
+  // your code here
+  var result = [];
+  for (i=0; i < arr.length; i++) {
+    result = result.concat(arr[i]);
+  }
+  return result;
+}
+
+function getProductOfAllElementsAtProperty(obj, key) {
+  // your code here
+  if (Array.isArray(obj[key]) && obj[key].length > 0) {
+    return obj[key].reduce((acc, val) => acc * val);
+  } else {
+    return 0;
+  }
+}
+
+function sumDigits(num) {
+  // your code here
+  var string = num.toString();
+  var nums = string.split('');
+  if (nums[0] === '-') {
+    nums.splice(0, 2, (nums[1] * -1));
+    nums[0] = nums[0].toString();
+  }
+  var result = nums.map(element => parseInt(element));
+  return result.reduce((acc, val) => acc + val);
+}
+
+var output = sumDigits(-316);
+console.log(output);
+
+function getSumOfAllElementsAtProperty(obj, key) {
+  // your code here
+  if (Array.isArray(obj[key]) && obj[key].length > 0) {
+    return obj[key].reduce((acc, val) => acc + val);
+  } else {
+    return 0;
+  }
+}
+
+/*
+function findShortestWordAmongMixedElements(arr) {
+  // your code here
+  var strings = arr.filter(element => typeof(element) === 'string');
+  if (strings.length > 0) {
+    var shortest = strings[0];
+    for (i = 0; i < strings.length; i++) {
+      if (strings[i].length < shortest.length) {
+        shortest = strings[i];
+      }
+    }
+    return shortest;
+  } else {
+    return '';
+  }
+}
+*/
+
+function findSmallestNumberAmongMixedElements(arr) {
+  // your code here
+  var nums = arr.filter(ele => typeof(ele) === 'number');
+  if (nums.length > 0) {
+    return Math.min(...nums);
+  } else {
+    return 0;
+  }
+}
+
+function getLongestWordOfMixedElements(arr) {
+  // your code here
+  var longest = "";
+  var words = arr.filter(ele => typeof(ele) === 'string');
+  words.forEach(function(word) {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  });
+  return longest;
+}
+
+function getLargestNumberAmongMixedElements(arr) {
+  // your code here
+  var largest = 0;
+  var nums = arr.filter(ele => typeof(ele) === 'number');
+  if (nums.length > 0) {
+    largest = Math.max(...nums);
+  }
+  return largest;
+}
+
+function computeSummationToN(n) {
+  var sum = 0;
+  for (i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function convertScoreToGrade(score) {
+  // your code here
+  if (score < 100 && score >= 90) {
+    return 'A';
+  } else if (score < 90 && score >= 80) {
+    return 'B';
+  } if (score < 80 && score >= 70) {
+    return 'C';
+  } if (score < 70 && score >= 60) {
+    return 'D';
+  } if (score < 60 && score >= 0) {
+    return 'F';
+  } else {
+    return 'INVALID SCORE';
+  }
+}
+
+function convertScoreToGradeWithPlusAndMinus(score) {
+  // your code here
+  var sign = '';
+  var range = score%10;
+  if (range > 7) {
+    sign = '+';
+  } else if (range <=7 && range > 2) {
+    sign = '';
+  } else {
+    sign = '-';
+  }
+
+  var grade = 'INVALID SCORE';
+  if (score < 100 && score >= 90) {
+    grade = 'A';
+  } else if (score < 90 && score >= 80) {
+    grade = 'B';
+  } if (score < 80 && score >= 70) {
+    grade = 'C';
+  } if (score < 70 && score >= 60) {
+    grade = 'D';
+  } if (score < 60 && score >= 0) {
+    grade = 'F';
+  }
+
+
+if (score === 100) {
+  return 'A+';
+} else if (grade !== 'INVALID SCORE' && score > 59) {
+  return (grade + sign);
+} else {
+  return grade;
+}
+
+}
+
+function computeFactorialOfN(n) {
+  // your code here
+    var fac = 1;
+    for (i = 2; i <= n; i++) {
+      fac *=i;
+    }
+  return fac;
+}
+
+function repeatString(string, num) {
+  // your code here
+  return string.repeat(num);
+}
+
+function getLongestOfThreeWords(word1, word2, word3) {
+  // your code here
+  var result = arguments[0];
+  for (i = 0; i < arguments.length; i++) {
+    if (arguments[i].length > result.length) {
+      result = arguments[i];
+    }
+  }
+  return result;
+}
+
+function findShortestOfThreeWords(word1, word2, word3) {
+  // your code here
+  var result = arguments[0];
+  for (i = 0; i < arguments.length; i++) {
+    if (arguments[i].length < result.length) {
+      result = arguments[i];
+    }
+  }
+  return result;
+}
+
+function computeCompoundInterest(principal, interestRate, compoundingFrequency, timeInYears) {
+  // your code here
+  var interestByFreq = interestRate/compoundingFrequency;
+  var i = (1 + interestByFreq) ** (compoundingFrequency * timeInYears);
+  console.log(i);
+  var I = (principal * i) - principal;
+  return I;
+}
+
+function modulo(num1, num2) {
+  // your code here
+  if ((!isNaN(num1) && !isNaN(num2)) && num2 !== 0) {
+    var remainder = Math.floor(Math.abs(num1/num2));
+    var modulo = Math.abs(num1) - (remainder * Math.abs(num2));
+    if (num1 < 0) {
+      return modulo * -1;
+    } else {
+      return modulo;
+    }
+  } else {
+    return 'NaN';
+  }
+}
+
+function multiply(num1, num2) {
+  // your code here
+  var result = 0;
+  for (i = 0; i < Math.abs(num2); i++) {
+    result += Math.abs(num1);
+  }
+  if (num1 < 0 || num2 < 0) {
+    result = -result;
+    return result;
+  } else {
+    return result;
+  }
+}
+
+function isOddWithoutModulo(num) {
+  // your code here
+  return (num/2 !== Math.round(num/2));
+}
+
+function isEvenWithoutModulo(num) {
+  // your code here
+  return (num/2 === Math.round(num/2));
+}
+
+function multiplyBetween(num1, num2) {
+  // your code here
+  var result = 0;
+  if (num1 < num2) {
+    result = 1;
+    for (i = num1; i < num2; i++) {
+      result *= i;
+    }
+  }
+  return result;
+}
+
+// exercise 144
+function computeSumBetween(num1, num2) {
+  // your code here
+  var result = 0;
+  if (num1 < num2) {
+    for (i = num1; i < num2; i++) {
+      result += i;
+    }
+  }
+  return result;
+}
